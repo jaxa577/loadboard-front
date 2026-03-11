c9be1825787929b2415d64a527b54e437423668a
jony-07 <javohir.habibjonov@webase.uz>
2026-03-12 01:52:39 +0500

    changed logo

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -4,7 +4,7 @@
       <div class="flex items-center justify-between h-16">
         <!-- Logo (Always Visible) -->
         <NuxtLink to="/" class="flex items-center z-10">
-          <img src="~/assets/LB.svg" alt="LoadBord Logo" class="h-16 w-auto" />
+          <img src="~/assets/LB.svg" alt="LoadBord Logo" class="h-14 w-auto" />
         </NuxtLink>
 
         <!-- Desktop Navigation -->
